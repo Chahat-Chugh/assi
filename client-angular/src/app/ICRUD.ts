f596659cff28b5f9af0920513aa4684acf81a7de
Chahat-Chugh <99480242+Chahat-Chugh@users.noreply.github.com>
2022-05-12 20:29:42 +0530

    Delete client-angular directory

--- client-angular/src/app/ICRUD.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface ICRUD<T> {
-  create(obj: T): Promise<T>;
-  getAll(): Promise<T[]>;
-  getOneBy(id: number): Promise<T>;
-  delete(obj: T): Observable<any>;
-  save(obj: T): Observable<any>;
-}
--- client-angular/src/app/role.model.ts
+++
@@ -1,9 +0,0 @@
-import { ROLE } from "./user.model";
-
-export class Role{
-    key!:ROLE;
-    name!:string;
-    description?:string;
-    createdOn?:string;
-    modifiedOn?:string;
-}
